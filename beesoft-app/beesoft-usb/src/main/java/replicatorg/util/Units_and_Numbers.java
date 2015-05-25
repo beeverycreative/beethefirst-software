66d7d0db566559cee9bab4fdff244071e38859c4
davidp <david.pacheco@beubi.com>
2015-05-25 12:08:13 +0100

    BEESOFT-125

--- beesoft-app/beesoft-usb/src/main/java/replicatorg/util/Units_and_Numbers.java
+++ beesoft-app/beesoft-usb/src/main/java/replicatorg/util/Units_and_Numbers.java
@@ -4,53 +4,65 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
-* Copyright (c) 2013 BEEVC - Electronic Systems
-* This file is part of BEESOFT software: you can redistribute it and/or modify 
-* it under the terms of the GNU General Public License as published by the 
-* Free Software Foundation, either version 3 of the License, or (at your option)
-* any later version. BEESOFT is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
-* for more details. You should have received a copy of the GNU General
-* Public License along with BEESOFT. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (c) 2013 BEEVC - Electronic Systems This file is part of BEESOFT
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version. BEESOFT is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+ * should have received a copy of the GNU General Public License along with
+ * BEESOFT. If not, see <http://www.gnu.org/licenses/>.
+ */
 public class Units_and_Numbers {
- 
+
     public static double millimetersToInches(double mm) {
-        
+
         return (mm * 0.0393701);
     }
-    
+
     public static double inchesToMillimeters(double inches) {
-        
+
         return (inches * 25.4);
     }
-    
+
     public static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
+        if (places < 0) {
+            throw new IllegalArgumentException();
+        }
 
         long factor = (long) Math.pow(10, places);
         value = value * factor;
         long tmp = Math.round(value);
         return (double) tmp / factor;
     }
-    
-    
-    public static double sGetDecimalStringAnyLocaleAsDouble (String value) {
 
-    Locale theLocale = Locale.getDefault();
-    NumberFormat numberFormat = DecimalFormat.getInstance(theLocale);
-    Number theNumber;
-    try {
-        theNumber = numberFormat.parse(value);
-        return theNumber.doubleValue();
-    } catch (ParseException e) {
-        //SKLogger.sAssert(SKCommon.class,  false);
+    public static double sGetDecimalStringAnyLocaleAsDouble(String value) {
 
-        String valueWithDot = value.replaceAll(",",".");
-        return Double.valueOf(valueWithDot);
+        try {
+            Locale theLocale = Locale.getDefault();
+            NumberFormat numberFormat = DecimalFormat.getInstance(theLocale);
+            Number theNumber;
+            String filteredValue = value;
+            
+            //We have to filter out any commas. Some locales just ignore the 
+            //commas and transform a number like 102,92 into 10292...
+            if (filteredValue.contains(",")) {
+                filteredValue = value.replaceAll(",", ".");
+            }
+
+            theNumber = numberFormat.parse(filteredValue);
+            return theNumber.doubleValue();
+            
+        } catch (ParseException ex) {
+            Logger.getLogger(Units_and_Numbers.class.getName()).log(Level.SEVERE, null, ex);
+            
+            return Double.valueOf(value);
+        }
+        
     }
-}
 }
