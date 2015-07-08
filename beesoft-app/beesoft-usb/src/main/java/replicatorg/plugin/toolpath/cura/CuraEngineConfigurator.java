355514488600d7cc0cf02cbcc6888b6abc82ac69
David Pacheco <david.pacheco@beubi.com>
2015-07-08 15:30:38 +0100

    BEESOFT-141

--- beesoft-app/beesoft-usb/src/main/java/replicatorg/plugin/toolpath/cura/CuraEngineConfigurator.java
+++ beesoft-app/beesoft-usb/src/main/java/replicatorg/plugin/toolpath/cura/CuraEngineConfigurator.java
@@ -223,7 +223,7 @@ public class CuraEngineConfigurator {
      * @return operation code.
      */
     public int readIni(File curaIniFile) {
-        String line = null;
+        String line;
         int error = 0;
         
         try {
@@ -271,11 +271,9 @@ public class CuraEngineConfigurator {
      * Maps INI attributes to a CFG file to be passed to CuraEngine.
      */
     public void mapIniToCFG() {
-        Iterator it = curaCfg.entrySet().iterator();
-
         // Reads CURA CFG Map to map parameter
-        while (it.hasNext()) {
-            Map.Entry pairs = (Map.Entry) it.next();
+        
+        for (Map.Entry pairs : curaCfg.entrySet()) {
             String cfgKey = pairs.getKey().toString();
             //(pairs.getKey() + "=" + pairs.getValue());
 
@@ -300,15 +298,9 @@ public class CuraEngineConfigurator {
     }
 
     private void printCFG() {
-        Iterator it = curaCfg.entrySet().iterator();
-
-        // Reads CURA CFG Map to map parameter
-        while (it.hasNext()) {
-            Map.Entry pairs = (Map.Entry) it.next();
-
-            String cfgKey = pairs.getKey().toString();
+        // Reads CURA CFG Map to map parameter        
+        for (Map.Entry pairs : curaCfg.entrySet()) {
             System.out.println(pairs.getKey() + "=" + pairs.getValue());
-
         }
     }
 
