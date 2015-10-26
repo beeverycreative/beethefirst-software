6be0f3703aec3414da8cc35ba48f6e63618a58af
João Grego <jpgrego@gmail.com>
2015-10-26 11:43:53 +0000

    Whoopsie

--- beesoft-app/beesoft-usb/src/main/java/pt/beeverycreative/beesoft/drivers/usb/UsbPassthroughDriver.java
+++ beesoft-app/beesoft-usb/src/main/java/pt/beeverycreative/beesoft/drivers/usb/UsbPassthroughDriver.java
@@ -1,6 +1,5 @@
 package pt.beeverycreative.beesoft.drivers.usb;
 
-import com.sun.xml.internal.ws.resources.SenderMessages;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
